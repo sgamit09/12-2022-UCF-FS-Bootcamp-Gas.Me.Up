25d7ed29c382d8809475632c7e90c82fefdb640b
GigaScott <scott.gamit.09@outlook.com>
2022-05-23 12:16:38 -0400

    updated dateFormat.js

---
+++ utils/dateFormat.js
@@ -0,0 +1,8 @@
+import dateFormat, { masks } from "dateformat";
+const now = new Date();
+
+//added dateformat npm = https://www.npmjs.com/package/dateformat
+
+// Basic usage
+dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+// Saturday, June 9th, 2007, 5:46:21 PM
